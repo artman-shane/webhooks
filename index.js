6f6cb45d411f94a6ee5a3559f785969a7ccec507
sartman <sartman@twilio.com>
2024-10-23 20:31:17 -0400

    Initial

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ngrok = require("ngrok");
+
+const app = express();
+const PORT = 5050;
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`running on port ${PORT}`);
+});
+
+app.use("/api", (req, res) => {
+  console.log("Headers", req.headers, "\n\n");
+  console.log("Body", req.body, "\n\n");
+  console.log("Query", req.query, "\n\n");
+  consosle.log(`TranscriptionData: ${req.query.TranscriptionData}`);
+  consosle.log(`Transcript: ${req.query.TranscriptionData.transcript}`);
+  res.status(200).end();
+});
+
+(async function () {
+  const url = await ngrok.connect({
+    addr: PORT,
+    subdomain: "shane",
+  });
+  console.log(`ngrok tunnel opened at ${url}`);
+})();
